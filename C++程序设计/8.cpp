d57f2ca551f60337002df9b2a43e5d6f3d2b15db
yuanyi2000 <yuanyi2000@akiyun.com>
2020-11-19 20:08:06 +0800

    homework

--- C++程序设计/8.cpp
+++ C++程序设计/8.cpp
@@ -15,7 +15,14 @@ void print(vector<int> v,string s)
 }
 
 // 3
-vector<int> fibonacci(int x,int y,vector<int> v,int  n)
+enum class Abc{
+    a,b,c
+};
+Abc operator++(Abc& e)
+{
+    e = (e==c)? a : Abc(int(e)+1);
+}
+void fibonacci(int x,int y,vector<int> v,int  n)
 {
     if(!v.empty) error("v is not empty!")
     if(n <= 1) error("n is lower than 2")    // n = 2 ??
@@ -28,5 +35,49 @@ vector<int> fibonacci(int x,int y,vector<int> v,int  n)
         v.push_back(next);
     }
     
-    return v;
+    string s = "";
+    e = Abc(3);
+    for(int i = 0;i < n;i++)
+        s.push_back(e++);
+        
+    print(v,s);
+    
+}
+
+// 5
+void swap_int(int * p1,int * p2)
+{
+    int * p3 = p1;
+    p1 = p2;
+    p2 = p3;
+}
+
+void swap_self(vector<int> v)
+{
+    mid = ((v.size()%2)==0) ? (v.size()/2-1) : (v.size()-1)/2  
+    for(int i = 0;i <= mid;i++)
+            swap_int(v[i], v[v.size()-i-1]);
+}
+
+int main()
+{
+    cout << "8,2/n";
+    vector<int> v2 {1, 2, 3, 4, 5 ,6};
+    string s2 = "string";
+    cout << "expected :s 1 t 2 r 3 i 4 n 5 g 6 /n/n"
+    print(v2, s2);
+    
+    cout << "/n/n";
+    
+    vector<int> v3;
+    fiboncci(1,1,v3,10);
+    
+    cout << "/n/n";
+    
+    swap_self(v2);
+    print(v2,s2);
+    
+    return 0;
+    
+    
 }
