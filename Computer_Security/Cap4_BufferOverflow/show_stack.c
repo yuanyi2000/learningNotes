559010857573ea57760ee46215bc0c228464ff36
yuanyi2000 <yuanyi2000@aliyun.com>
2020-12-28 19:31:08 +0800

    进度搁置

---
+++ Computer_Security/Cap4_BufferOverflow/show_stack.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void func(int a, int b){
+    int x, y;
+
+    x = a + b;
+    y = a - b;
+    return;
+}
+
+int main()
+{
+    int m = 3;
+    int n = 4;
+
+    func(m, n);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
